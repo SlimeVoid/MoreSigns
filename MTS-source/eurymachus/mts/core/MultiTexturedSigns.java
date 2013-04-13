786ca805ad4c074bf75fe3c13b9205b7eb7495d9
Eurymachus <reflex_ion@hotmail.com>
2013-04-13 03:40:20 +0100

    Version Update

--- MTS-source/eurymachus/mts/core/MultiTexturedSigns.java
+++ MTS-source/eurymachus/mts/core/MultiTexturedSigns.java
@@ -16,7 +16,7 @@ import eurymachus.mts.network.MTSConnection;
 		modid = "MultiTexturedSigns",
 		name = "Multi-Textured Signs",
 		dependencies = "after:SlimevoidLib",
-		version = "2.0.0.1")
+		version = "2.1.0.0")
 @NetworkMod(
 		clientSideRequired = true,
 		serverSideRequired = false,

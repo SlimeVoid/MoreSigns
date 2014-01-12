3049d29aa91bd2062e756b5e836f0927110f2f8d
Eurymachus <reflex_ion@hotmail.com>
2014-01-12 14:30:11 +0000

    Version 2.1.1.1

--- MS-source/slimevoid/moresigns/core/lib/CoreLib.java
+++ MS-source/slimevoid/moresigns/core/lib/CoreLib.java
@@ -5,7 +5,7 @@ public class CoreLib {
 	public static final String	MOD_ID				= "MoreSigns";
 	public static final String	MOD_RESOURCES		= "moresigns";
 	public static final String	MOD_NAME			= "More Signs Mod";
-	public static final String	MOD_VERSION			= "2.1.1.0";
+	public static final String	MOD_VERSION			= "2.1.1.1";
 	public static final String	MOD_DEPENDENCIES	= "after:SlimevoidLib";
 	public static final String	MOD_CHANNEL			= "MORESIGNS";
 	public static final String	CLIENT_PROXY		= "slimevoid.moresigns.client.proxy.ClientProxy";

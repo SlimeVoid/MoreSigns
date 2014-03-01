a68881a1e3328264c0bfa7af36583a9f2c744e7f
Eurymachus <reflex_ion@hotmail.com>
2014-03-01 15:09:32 +0000

    Fixed search-replace derp

--- src/main/java/com/slimevoid/moresigns/core/lib/CoreLib.java
+++ src/main/java/com/slimevoid/moresigns/core/lib/CoreLib.java
@@ -6,8 +6,8 @@ public class CoreLib {
 	public static final String	MOD_RESOURCES		= "moresigns";
 	public static final String	MOD_NAME			= "More Signs Mod";
 	public static final String	MOD_VERSION			= "2.1.1.3";
-	public static final String	MOD_DEPENDENCIES	= "required-after:com.slimevoid.library";
+	public static final String	MOD_DEPENDENCIES	= "required-after:SlimevoidLib";
 	public static final String	MOD_CHANNEL			= "MORESIGNS";
-	public static final String	CLIENT_PROXY		= "slimevoid.moresigns.client.proxy.ClientProxy";
-	public static final String	COMMON_PROXY		= "slimevoid.moresigns.proxy.CommonProxy";
+	public static final String	CLIENT_PROXY		= "com.slimevoid.moresigns.client.proxy.ClientProxy";
+	public static final String	COMMON_PROXY		= "com.slimevoid.moresigns.proxy.CommonProxy";
 }
